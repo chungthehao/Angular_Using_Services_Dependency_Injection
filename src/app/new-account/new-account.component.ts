bed9923124a63ac53a361f0bd9ace6f07c684777
Henry Chung <chungthehao@gmail.com>
2019-04-03 15:08:25 +0700

    Creating a Logging Service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-new-account',
@@ -13,6 +14,11 @@ export class NewAccountComponent {
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+
+    // Cach dung SAI
+    const logService = new LoggingService();
+    logService.logStatusChange(accountStatus);
+    
+    // console.log('A server status changed, new status: ' + accountStatus);
   }
 }
